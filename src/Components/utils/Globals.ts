2901500374b3643507a49433730ce80e97fe71e9
Denisiuss <97143436+Denisiuss@users.noreply.github.com>
2022-01-30 03:13:27 +0200

    Update Globals.ts

--- src/Components/utils/Globals.ts
+++ src/Components/utils/Globals.ts
@@ -2,14 +2,14 @@ class Globals{}
 
 class DevelopmentGlobals extends Globals{
     public urls = {
-        administrator : "https://coupon-system-back.herokuapp.com/admin/",
-        //administrator : "http://localhost:8080/admin/",
-        company : "https://coupon-system-back.herokuapp.com/company/",
-        //company : "http://localhost:8080/company/",
-        customer : "https://coupon-system-back.herokuapp.com/customer/",
-        //customer : "http://localhost:8080/customer/",
-        guest : "https://coupon-system-back.herokuapp.com/guest/"
-        //guest : "http://localhost:8080/guest/"
+        //administrator : "https://coupon-system-back.herokuapp.com/admin/",
+        administrator : "http://localhost:8080/admin/",
+        //company : "https://coupon-system-back.herokuapp.com/company/",
+        company : "http://localhost:8080/company/",
+        //customer : "https://coupon-system-back.herokuapp.com/customer/",
+        customer : "http://localhost:8080/customer/",
+        //guest : "https://coupon-system-back.herokuapp.com/guest/"
+        guest : "http://localhost:8080/guest/"
     }
 }
 
